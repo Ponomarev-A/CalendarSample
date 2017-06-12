366787e314c32195afc24f8c1ba2eca67cd97cea
Ponomarev A. (work) <gpwaic@gmail.com>
2017-06-12 13:37:44 +0300

    added calendar event details viewing activity

---
+++ app/src/main/java/com/sbrt/ponomarev/calendarsample/ui/CalendarEvent.java
@@ -0,0 +1,14 @@
+package com.sbrt.ponomarev.calendarsample.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.sbrt.ponomarev.calendarsample.R;
+
+public class CalendarEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_event);
+    }
+}
