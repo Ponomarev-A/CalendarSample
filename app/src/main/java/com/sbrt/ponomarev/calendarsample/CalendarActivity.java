896823572b2039c5e3fd1810799cd2d26208d06d
Ponomarev A. (work) <gpwaic@gmail.com>
2017-06-12 12:07:11 +0300

    added requesting permissions

--- app/src/main/java/com/sbrt/ponomarev/calendarsample/CalendarActivity.java
+++ app/src/main/java/com/sbrt/ponomarev/calendarsample/CalendarActivity.java
@@ -1,13 +1,41 @@
 package com.sbrt.ponomarev.calendarsample;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CalendarActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CODE = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        checkCalendarPermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) == PackageManager.PERMISSION_GRANTED) {
+                // TODO: load calender events..
+            }
+        }
+    }
+
+    private void checkCalendarPermission() {
+        int permissionResult = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CALL_LOG);
+        if (permissionResult == PackageManager.PERMISSION_GRANTED) {
+            // TODO: load calender events..
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_CALENDAR}, PERMISSION_REQUEST_CODE);
+        }
     }
 }
