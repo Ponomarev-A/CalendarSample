231b85ad742d3086c3ba3e26564b8de6522ecd71
Ponomarev A. (work) <gpwaic@gmail.com>
2017-06-13 15:09:23 +0300

    impl DAO operations

--- app/src/main/java/com/sbrt/ponomarev/calendarsample/data/CalendarDAO.java
+++ app/src/main/java/com/sbrt/ponomarev/calendarsample/data/CalendarDAO.java
@@ -1,5 +1,6 @@
 package com.sbrt.ponomarev.calendarsample.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.provider.CalendarContract;
@@ -7,6 +8,7 @@ import com.sbrt.ponomarev.calendarsample.utlis.CalendarUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  * Created by Ponomarev on 13.06.2017.
@@ -14,6 +16,7 @@ import java.util.List;
 
 public class CalendarDAO {
 
+    private static final int DEFAULT_CALENDAR_ID = 1;
     private Context context;
 
     public CalendarDAO(Context context) {
@@ -46,27 +49,47 @@ public class CalendarDAO {
         return events;
     }
 
+    @SuppressWarnings("ResourceType")
     public long insertCalendarEvent(CalendarEvent event) {
-        // TODO: Don't forget implement method
-        return 1;
+        return context.getContentResolver().insert(CalendarContract.Events.CONTENT_URI, createContentValuesFromEvent(event)) != null ?
+                1 : 0;
     }
 
+    @SuppressWarnings("MissingPermission")
     public int deleteCalendarEvent(long id) {
-        // TODO: Don't forget implement method
-        return 1;
+        return context.getContentResolver().delete(CalendarContract.Events.CONTENT_URI,
+                CalendarContract.Events._ID + "=?",
+                new String[]{String.valueOf(id)}
+        );
     }
 
+    @SuppressWarnings("MissingPermission")
     public int updateCalendarEvent(CalendarEvent event) {
-        // TODO: Don't forget implement method
-        return 1;
+        return context.getContentResolver().update(CalendarContract.Events.CONTENT_URI,
+                createContentValuesFromEvent(event),
+                CalendarContract.Events._ID + "=?",
+                new String[]{String.valueOf(event.id)}
+        );
+    }
+
+    private ContentValues createContentValuesFromEvent(CalendarEvent event) {
+        ContentValues values = new ContentValues();
+        values.put(CalendarContract.Events._ID, event.id);
+        values.put(CalendarContract.Events.TITLE, event.title);
+        values.put(CalendarContract.Events.DESCRIPTION, event.description);
+        values.put(CalendarContract.Events.DTSTART, event.dtstart);
+        values.put(CalendarContract.Events.DTEND, event.dtend);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getID());
+        values.put(CalendarContract.Events.CALENDAR_ID, DEFAULT_CALENDAR_ID);
+        return values;
     }
 
     @SuppressWarnings("MissingPermission")
     private Cursor getCalendarEventCursor(Long id) {
-        return context.getContentResolver().query( CalendarContract.Events.CONTENT_URI,
+        return context.getContentResolver().query(CalendarContract.Events.CONTENT_URI,
                 null,
                 id == null ? null : CalendarContract.Events._ID + "=?",
-                id == null ? null : new String[] {String.valueOf(id)},
+                id == null ? null : new String[]{String.valueOf(id)},
                 null
         );
     }
